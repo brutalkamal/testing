c2538360463723e0e65ae7f7d10dda296ca5564b
brutalkamal <brutalkamal987@gmail.com>
2023-04-29 11:54:29 +0530

    sample

---
+++ src/org/data/CompanyData.java
@@ -0,0 +1,79 @@
+package org.data;
+
+import java.util.Comparator;
+
+public class CompanyData {
+	
+
+	public static void main(String[]args) {
+		String d=new String("Book Hotel - Royal Orchid Resort & Convention Centre Standard");
+		String substring = d.substring(13);
+		System.out.println(substring);
+		String b=new String("kamal");
+		String intern = b.intern();
+		System.out.println(intern);
+		int indexOf = b.indexOf(1, 1);
+		System.out.println(indexOf);
+		
+		
+		
+		
+//		String a=new String("kamalraj");
+//		String b=new String("SUNDHARAM");
+//		String c=new String(" kalai Raja");
+//		
+//		char charAt = a.charAt(5);
+//		System.out.println(charAt);
+//		
+//		int indexOf=c.indexOf('l');
+//		System.out.println(indexOf);
+//		
+//		int lastIndexOf = a.lastIndexOf('a');
+//		System.out.println(lastIndexOf);
+//		
+//		String toUpperCase= c.toUpperCase();
+//		System.out.println(toUpperCase);
+//		    
+//		String toLowerCase= b.toLowerCase();
+//	    System.out.println(toLowerCase);
+//	    
+//		int length=a.length();
+//	    System.out.println(length);
+//	    
+//	    boolean contains = b.contains("SUNDHARAM");
+//	    System.out.println(contains);
+//	    
+//	    boolean isEmpty=c.isEmpty();
+//		System.out.println(isEmpty);
+//		
+//		boolean startsWith = a.startsWith("kamal");
+//		System.out.println(startsWith);
+//		
+//		boolean endsWith = a.endsWith("j");
+//		System.out.println(endsWith);
+//		
+//		boolean equals= a.equals("kamalRaj");
+//	    System.out.println(equals);
+//		
+//	    boolean equalsIgnoreCase= a.equalsIgnoreCase("kamalraj");
+//	    System.out.println(equalsIgnoreCase);
+//	    
+//	    int compareTo = a.compareTo(b);
+//	    System.out.println(compareTo);
+//		   
+//	    String replace = a.replace("a", "s");
+//		System.out.println(replace);
+//		   
+//	    String replaceAll = b.replaceAll("A", "s");
+//	    System.out.println(replaceAll);
+//
+//    	String trim = c.trim();
+//	    System.out.println(trim);
+//		  
+//	    String substring = a.substring(2);
+//	    System.out.println(substring);
+//		  
+//	    String substring2 = b.substring(1, 5);
+//	    System.out.println(substring2);
+	    }
+   }
